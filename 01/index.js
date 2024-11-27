b18bd99fea1d7e0467e06e1638ac9322678ca455
Begmatov Almaz <lolkek13@inbox.ru>
2024-11-27 13:31:16 +0600

    Added echo function

--- 01/index.js
+++ 01/index.js
@@ -7,8 +7,8 @@ const port = 8000
 app.get('/', (req, res) => {
     return res.send(' <h1> Home page </h1>')
 })
-app.get('/Hello', (req, res) => {
-    return res.send(' <h1> Hello  </h1>')
+app.get('/:userMessage', (req, res) => {
+    return res.send(` <h1>${req.params.userMessage}</h1>`)
 })
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
