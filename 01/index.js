5fa0b13395580e9bed3e7e186320b8766e064df8
Begmatov Almaz <lolkek13@inbox.ru>
2024-11-27 18:34:10 +0600

    Added encode password

--- 01/index.js
+++ 01/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const Caesar = require('caesar-salad').Caesar;
 
 const app = express()
 
 const port = 8000
+
+
 
 app.get('/', (req, res) => {
     return res.send(' <h1> Home page </h1>')
@@ -10,6 +13,12 @@ app.get('/', (req, res) => {
 app.get('/:userMessage', (req, res) => {
     return res.send(` <h1>${req.params.userMessage}</h1>`)
 })
+
+app.get(`/encode/:password`, (req, res) => {
+    const ceasar = Caesar.Cipher('b').crypt(req.params.password);
+    return res.send(` <h1>Encodeded: ${ceasar}</h1>`)
+})
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
